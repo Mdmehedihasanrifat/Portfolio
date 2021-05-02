8f5c895212752d3fd7f73a8d7145422af7ee119b
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2021-05-03 04:11:48 +0600

    hamburger

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -4,23 +4,27 @@ import PersonIcon from '@material-ui/icons/Person';
 import Mail from '@material-ui/icons/Mail';
 const Topbar = () => {
     return (
-        <div className="topbar">
+        <div className="topbar active ">
          <div className="wrapper">
-                <div className="left">
+                <div className="left ">
                     <a href="#intro" className="logo">Md Mehedi Hasan.</a>
                     <div className="itemContainer">
-                    <PersonIcon></PersonIcon>
+                    <PersonIcon className="icon"></PersonIcon>
                         <span>+8801683952127</span>
                     </div>
                     <div className="itemContainer">
-                        <Mail></Mail>
+                        <Mail className="icon"></Mail>
                         <span>mehedi.rifat@gmail.com</span>
                     </div>
 
                 </div>
 
              <div className="right">
-                 <a href="#intro">intro</a>
+                 <div className="hamburger">
+                     <span className="line1"></span>
+                     <span className="line2"></span>
+                     <span className="line3"></span>
+                 </div>
              </div>
          </div>
         </div>
