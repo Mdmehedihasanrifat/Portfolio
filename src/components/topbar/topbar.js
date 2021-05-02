7a3f8ab394b689b36f9a0134d7bd1422506dfd36
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2021-05-02 21:07:43 +0600

    protfolio icon

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './topbar.css'
 import PersonIcon from '@material-ui/icons/Person';
+import Mail from '@material-ui/icons/Mail';
 const Topbar = () => {
     return (
         <div className="topbar">
          <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Md Mehedi Hasan.</a>
-                    <div className="itemContainer"></div>
-                    <PersonIcon><span>+8990***</span></PersonIcon>
+                    <div className="itemContainer">
+                    <PersonIcon></PersonIcon>
+                        <span>+8801683952127</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail></Mail>
+                        <span>mehedi.rifat@gmail.com</span>
+                    </div>
+
                 </div>
+
              <div className="right">
                  <a href="#intro">intro</a>
              </div>
